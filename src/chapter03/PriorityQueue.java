5f1b78ae89d0cffd6bc2a246e0d2663813c03334
gwaifaiwong <wang8guihui@gmail.com>
2020-07-16 00:20:30 +0800

    3.4.2 优先队列的实现

--- src/chapter03/PriorityQueue.java
+++ src/chapter03/PriorityQueue.java
@@ -86,7 +86,7 @@ public class PriorityQueue {
                 childIndex++;
             }
             //如果父节点小于任何一个孩子的值，直接跳出
-            if (temp <= array[childIndex]) {
+            if (temp >= array[childIndex]) {
                 break;
             }
             //无需真正交换，单向赋值即可
